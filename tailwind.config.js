a2fc04edfb157f626432ede6c973f7722a83d920
Omar Labana <omarlabana@outlook.com>
2021-12-06 15:50:17 +0200

    Add Tailwind CSS - test Ar text

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['Cairo', 'sans-serif']
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ text.js
@@ -0,0 +1,12 @@
+const HeaderNavigation = {
+    main: 'الرئيسية',
+    categories: 'التصنيفات',
+    applications: 'التطبيقات',
+    deveopment: 'طور تطبيق',
+}
+
+const HeroHeader = 'طور متجرك و وسع آفاق تجارتك مع سوق تطبيقات زد';
+
+const HeroSubHeader = 'طور تجارتك من خلال تطبيقات وحلول سوق تطبيقات زد التي تساعدك في نعو أرباحك عبر إدارة عمليات متجرك بشكل فعال واحترافي';
+
+console.log(HeroSubHeader)
